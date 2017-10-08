fcf65b65f12734e7513e81f0913d7d26d248d717
Brett Schules <brettschules@gmail.com>
2017-10-08 14:28:30 -0400

    Added starting and running watts data

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -6,32 +6,36 @@ export default class Home extends Component {
     super()
     this.state = {
       checkboxes: ["hey"],
-      wattsAmount: 0,
+      runningWatts: 0,
+      startingWatts: 0,
       checkboxesResuls: ""
     }
   }
 
   handleInputChange = (event) => {
-    let convertToInt = parseInt(event.target.value)
+    let convertRunningWattsToInt = parseInt(event.target.dataset.runningwatts, 10)
+    let convertStartingWattsToInt = parseInt(event.target.dataset.startingwatts, 10)
     this.setState({
-      checkboxesResuls: {...this.state.checkboxesResuls, [event.target.name]: convertToInt},
-      wattsAmount: event.target.checked ? this.state.wattsAmount += convertToInt : this.state.wattsAmount -= convertToInt
+      checkboxesResuls: {...this.state.checkboxesResuls, [event.target.name]: [convertRunningWattsToInt, convertStartingWattsToInt]},
+      runningWatts: event.target.checked ? this.state.runningWatts += convertRunningWattsToInt : this.state.runningWatts -= convertRunningWattsToInt,
+      startingWatts: event.target.checked ? this.state.startingWatts += convertStartingWattsToInt : this.state.startingWatts -= convertStartingWattsToInt
     })
   }
 
   render() {
-    console.log(this.state.wattsAmount)
+    console.log(this.state.runningWatts)
+    console.log(this.state.startingWatts)
     return (
       <div>
         <div class="container">
           <ul>
             <h3>Checkbox Examples</h3>
               <li>
-                <input id="checkbox1" class="checkbox" type="checkbox" name="one" value = "1000" onChange={this.handleInputChange}/>
+                <input id="checkbox1" class="checkbox" type="checkbox" name="one" data-runningWatts = "1000" data-startingWatts = "1500" onChange={this.handleInputChange}/>
                 <label for="checkbox1">Label for checkbox 1 here in the flesh Label for checkbox 1 here in the flesh Label for checkbox 1 here in the flesh  </label>
               </li>
               <li>
-                <input id="checkbox2" class="checkbox" type="checkbox" name="two" value = "2000" onChange={this.handleInputChange}/>
+                <input id="checkbox2" class="checkbox" type="checkbox" name="two" data-runningWatts = "2000" data-startingWatts = "2500" onChange={this.handleInputChange}/>
                 <label for="checkbox2">Label for 2</label>
               </li>
             </ul>
