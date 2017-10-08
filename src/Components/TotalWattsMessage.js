ed1998e8ed7bf520936b8688854c481548d215df
Brett Schules <brettschules@gmail.com>
2017-10-08 19:27:34 -0400

    Working site, now need to do styling...

--- src/Components/TotalWattsMessage.js
+++ src/Components/TotalWattsMessage.js
@@ -7,11 +7,11 @@ export default class TotalWattsMessage extends Component{
 
   render() {
     return(
-      <div className="message"> 
-        <h1>You will need</h1>
-        <h1>{this.props.runningWatts} running watts</h1>
-        <h1>plus {this.props.runningWatts} starting watts</h1>
-        <h1>for a total of {this.props.totalStartingWatts} watts </h1>
+      <div className="message">
+        <h1 id="need">You will need</h1>
+        <h1 id="running-watts">{this.props.runningWatts.toLocaleString()} running watts</h1>
+        <h1 id="starting-watts">plus {this.props.startingWatts.toLocaleString()} starting watts</h1>
+        <h1 id="total-watts">for a total of {this.props.totalStartingWatts.toLocaleString()} watts </h1>
       </div>
     )
   }
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -35,24 +35,28 @@ export default class Home extends Component {
       <div>
         <div className="container">
           <TotalWattsMessage runningWatts = {this.state.runningWatts} startingWatts = {this.state.startingWatts} totalStartingWatts = {this.state.totalStartingWatts} />
+          <div className="recreational-use">
           <ul>
             <h3>Recreational Use</h3>
               {DataAppliancesAPI["Recreational Use"].map(Recreational =>
                 <Checkboxes title = {Recreational.title} watts={Recreational.watts} handleInputChange={this.handleInputChange}/>
               )}
           </ul>
-          <ul>
+          </div>
+          <ul className="storm-emergency-use">
             <h3>Storm Emergency Use</h3>
               {DataAppliancesAPI["Storm Emergency Use"].map(Storm =>
                 <Checkboxes title = {Storm.title} watts={Storm.watts} handleInputChange={this.handleInputChange}/>
               )}
           </ul>
+          <div className="jobsite">
           <ul>
             <h3>Jobsite</h3>
               {DataAppliancesAPI["Jobsite"].map(Jobsite =>
                 <Checkboxes title = {Jobsite.title} watts={Jobsite.watts} handleInputChange={this.handleInputChange}/>
               )}
           </ul>
+          </div>
         </div>
       </div>
     )
