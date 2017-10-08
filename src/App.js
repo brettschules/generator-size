04877a142aca1386aef53892c9eba24c17605f62
Brett Schules <brettschules@gmail.com>
2017-10-08 11:40:57 -0400

    Created app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+        Hi
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
