987d3f49a0b6e2eef5efd650d38a22a6732648b2
Brett Schules <brettschules@gmail.com>
2017-11-10 10:02:44 -0500

    about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,11 @@ export default class About extends Component {
   render(){
     return(
       <div className="about">
-        <h1>This is the About Page</h1>
+        <h1>About</h1>
+        <p>Calculategenerator.com is free to use.  It allows for our users to
+          be able to calcuate their wattage needs for their generator.  Please note
+          that this website is only for estimation purposes and may not be 100 percent accurate.
+        </p>
       </div>
     )
   }
